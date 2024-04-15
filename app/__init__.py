9baa83b6480ee848fb1fba4e622f48bbd903bdff
DiegoCoto29 <123786897+DiegoCoto29@users.noreply.github.com>
2024-04-14 21:41:43 -0600

    productos en init

--- app/__init__.py
+++ app/__init__.py
@@ -254,3 +254,52 @@ def editarSubCategoria(id_subcategoria):
     cursor.close()
     conn.close()
     return render_template('/admin/EditSubCategory.html', categorias= categoriasF, subcategoria=subcategoria)
+
+
+####################PRODUCTOS########################
+
+@app.route('/productos1')
+def productos1():
+    conn, cursor = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM productos")
+    productos = cursor.fetchall()
+    cursor.close()
+    return render_template('productos.html', productos=productos)
+
+# Ruta para crear un nuevo producto
+@app.route('/crear_producto', methods=['POST'])
+def crear_producto():
+    if request.method == 'POST':
+        conn, cursor = get_db_connection()
+        data = request.form
+        cursor = conn.cursor()
+        cursor.callproc("crear_producto", [data['id_producto'], data['nombre'], data['descripcion'], data['proveedor'], data['precio'], data['descuento'], data['cantidad_stock']])
+        cursor.close()
+        conn.commit()
+    return productos()
+
+# Ruta para actualizar un producto
+@app.route('/actualizar_producto', methods=['POST'])
+def actualizar_producto():
+    if request.method == 'POST':
+        conn, cursor = get_db_connection()
+        data = request.form
+        cursor = conn.cursor()
+        cursor.callproc("actualizar_producto", [data['id_producto'], data['nombre'], data['descripcion'], data['proveedor'], data['precio'], data['descuento'], data['cantidad_stock']])
+        cursor.close()
+        conn.commit()
+    return productos()
+
+# Ruta para eliminar un producto
+@app.route('/eliminar_producto/<int:id_producto>')
+def eliminar_producto(id_producto):
+    conn, cursor = get_db_connection()
+    cursor = conn.cursor()
+    cursor.callproc("eliminar_producto", [id_producto])
+    cursor.close()
+    conn.commit()
+    return productos()
+
+if __name__ == '__main__':
+    app.run(debug=True)
